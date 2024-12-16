a9f6990bafbac85d510a18ce32576323b9f63993
NagaramyaSails <103425769+NagaramyaSails@users.noreply.github.com>
2024-12-16 14:40:09 +0530

    testing

---
+++ Test-Walmart-Buybox-notification/Services/ILoggerService.cs
@@ -0,0 +1,18 @@
+﻿using Google.Api;
+using Google.Cloud.Logging.Type;
+using Google.Cloud.Logging.V2;
+using ShopifyOrderSupplierMapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopifyOrderSupplierMapping.Services
+{
+    public interface ILoggerService
+    {
+        Task WriteLogEntry(string message, LogSeverity logSeverity);
+    }
+}
+
